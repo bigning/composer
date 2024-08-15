23277eb0f33cfadeb50737abddd08d8c79b3d365
Ning Wang <ning.wang@databricks.com>
2024-08-15 14:43:40 -0700

    a

--- composer/trainer/trainer.py
+++ composer/trainer/trainer.py
@@ -2644,12 +2644,10 @@ class Trainer:
             for batch_idx, self.state.batch in enumerate(self._iter_dataloader(TrainerMode.TRAIN)):
                 # bigning debug nccl timeout
                 # bigning debug
-                """
                 t = torch.tensor([2, 2, 3], device=f'cuda:{dist.get_local_rank()}')
                 log.debug("bigning debug manually all reduce")
                 torch.distributed.all_reduce(t)
                 return
-                """
 
                 # Spin dataloader forward unless dataloader handles internally with dataset_resumption
                 if self.spin_dataloaders and 'train' not in self.state.dataset_resumption and batch_idx < int(
