0628dacd30499d0d784d0e9f25445be258f48acd
Ning Wang <ning.wang@databricks.com>
2024-08-15 17:04:11 -0700

    a

--- composer/core/engine.py
+++ composer/core/engine.py
@@ -233,7 +233,8 @@ class Engine():
             log.debug(f"bigning debug atexit run")
             self._close(state, logger)
             log.debug(f"bigning debug os exit")
-            os._exit(1) 
+            torch.distributed.destroy_process_group()
+            #os._exit(1) 
 
         atexit.register(my_fun)
 
